43722e3e6f9d3ced365d4600428696c080cd4f42
Lazar Nagulov <lazarnagulov1@gmail.com>
2024-11-24 15:20:43 +0100

    feat: Add update category proposals

--- app/src/main/java/com/eventorium/presentation/viewmodels/CategoryProposalViewModel.java
+++ app/src/main/java/com/eventorium/presentation/viewmodels/CategoryProposalViewModel.java
@@ -1,18 +1,32 @@
 package com.eventorium.presentation.viewmodels;
 
+import android.os.Build;
+
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.eventorium.data.models.Category;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class CategoryProposalViewModel extends ViewModel {
     private final MutableLiveData<List<Category>> categoryProposals = new MutableLiveData<>();
+    private final MutableLiveData<List<Category>> existingCategories = new MutableLiveData<>();
     private final MutableLiveData<Category> selectedCategory = new MutableLiveData<>();
 
     public CategoryProposalViewModel() {
         categoryProposals.setValue(List.of(
+                new Category("Electronics", "Devices and gadgets such as smartphones, laptops, and accessories."),
+                new Category("Clothing", "Apparel and fashion items for men, women, and children."),
+                new Category("Food & Beverages", "Groceries, packaged foods, and drinks."),
+                new Category("Home Appliances", "Appliances and tools for home use, such as refrigerators, washing machines, etc."),
+                new Category("Health & Wellness", "Products related to fitness, medicine, and personal well-being."),
+                new Category("Automotive", "Cars, motorcycles, spare parts, and automotive accessories.")
+        ));
+        existingCategories.setValue(List.of(
                 new Category("Electronics", "Devices and gadgets such as smartphones, laptops, and accessories."),
                 new Category("Clothing", "Apparel and fashion items for men, women, and children."),
                 new Category("Food & Beverages", "Groceries, packaged foods, and drinks."),
@@ -26,6 +40,10 @@ public class CategoryProposalViewModel extends ViewModel {
         return categoryProposals;
     }
 
+    public MutableLiveData<List<Category>> getExistingCategories() {
+        return existingCategories;
+    }
+
     public MutableLiveData<Category> getSelectedCategory() {
         return selectedCategory;
     }
@@ -34,11 +52,34 @@ public class CategoryProposalViewModel extends ViewModel {
         this.categoryProposals.setValue(categoriesProposals);
     }
 
+    public void setExistingCategories(List<Category> categories) {
+        this.categoryProposals.setValue(categories);
+    }
+
     public void setSelectedCategory(Category category) {
         selectedCategory.setValue(category);
     }
 
     public void updateCategory(Category updateCategory) {
 
+    }
+
+    public List<String> getExistingCategoriesName() {
+        List<String> result = new ArrayList<>();
+        result.add("");
+        List<String> categoriesNames;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            categoriesNames = Objects.requireNonNull(existingCategories.getValue())
+                    .stream()
+                    .map(Category::getName)
+                    .toList();
+        } else {
+            categoriesNames = Objects.requireNonNull(existingCategories.getValue())
+                    .stream()
+                    .map(Category::getName)
+                    .collect(Collectors.toList());
+        }
+        result.addAll(categoriesNames);
+        return result;
     }
 }
