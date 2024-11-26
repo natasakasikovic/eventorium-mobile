e105fbb64308ed70076ff6ce5fdc6bd2f9df5094
Ana Šinik <sinikana7@gmail.com>
2024-11-26 12:14:49 +0100

    feat: create edit company screen layout

--- app/src/main/java/com/eventorium/presentation/fragments/company/CompanyDetailsFragment.java
+++ app/src/main/java/com/eventorium/presentation/fragments/company/CompanyDetailsFragment.java
@@ -2,7 +2,11 @@ package com.eventorium.presentation.fragments.company;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -30,5 +34,15 @@ public class CompanyDetailsFragment extends Fragment {
                              Bundle savedInstanceState) {
         binding = FragmentCompanyDetailsBinding.inflate(inflater, container, false);
         return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.editCompanyButton.setOnClickListener(v -> {
+            NavController navController = Navigation.findNavController(view);
+            navController.navigate(R.id.action_companyDetails_to_editCompanyFragment);
+        });
     }
 }
