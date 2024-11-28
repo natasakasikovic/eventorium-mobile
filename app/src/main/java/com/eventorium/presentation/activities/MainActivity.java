a266b0b833e753556f3db4b39ba0ca70d3252796
Natasa Kasikovic~ <nkasikovic@yahoo.com>
2024-11-28 23:46:31 +0100

    feat: Implement UI for service booking

--- app/src/main/java/com/eventorium/presentation/activities/MainActivity.java
+++ app/src/main/java/com/eventorium/presentation/activities/MainActivity.java
@@ -131,6 +131,8 @@ public class MainActivity extends AppCompatActivity {
             navController.navigate(R.id.invitationsFragment);
         } else if (id == R.id.nav_categories) {
             navController.navigate(R.id.categoryOverviewFragment);
+        } else if (id == R.id.nav_booking){
+            navController.navigate(R.id.bookingFragment);
         }
         else {
             showBottomNavigation();
