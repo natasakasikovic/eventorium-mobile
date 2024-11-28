2965372bc6b65ac96c2f3995171254448b29d126
Ana Šinik <sinikana7@gmail.com>
2024-11-28 21:46:36 +0100

    feat: implement fav events overview fragment

--- app/src/main/java/com/eventorium/presentation/fragments/event/FavouriteEventsFragment.java
+++ app/src/main/java/com/eventorium/presentation/fragments/event/FavouriteEventsFragment.java
@@ -11,11 +11,46 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.eventorium.R;
+import com.eventorium.data.models.Event;
+import com.eventorium.databinding.FragmentFavouriteEventsBinding;
+import com.eventorium.presentation.adapters.EventsAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FavouriteEventsFragment extends Fragment {
+
+    private FragmentFavouriteEventsBinding binding;
+    private static final List<Event> events = new ArrayList<>();
+
+    public FavouriteEventsFragment() { }
+
+    public static FavouriteEventsFragment newInstance() {
+        return new FavouriteEventsFragment();
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_favourite_events, container, false);
+        binding = FragmentFavouriteEventsBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        prepareEventData();
+        binding.eventsRecycleView.setAdapter(new EventsAdapter(events));
+    }
+
+    public void prepareEventData() {
+        events.clear();
+        events.add(new Event("Concert",  "Novi Sad", R.drawable.conference));
+        events.add(new Event("Conference",  "Novi Sad", R.drawable.conference));
+        events.add(new Event("Workshop",  "Novi Sad", R.drawable.conference));
+        events.add(new Event("Festival",  "Novi Sad", R.drawable.conference));
+        events.add(new Event("Webinar", "Novi Sad", R.drawable.conference));
+        events.add(new Event("Webinar", "Novi Sad", R.drawable.conference));
+        events.add(new Event("Webinar", "Novi Sad", R.drawable.conference));
     }
 }
