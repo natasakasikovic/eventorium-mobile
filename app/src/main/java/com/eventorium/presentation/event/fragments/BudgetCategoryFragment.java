25002746780830720c34778874712420ca7597da
Lazar Nagulov <lazarnagulov1@gmail.com>
2024-12-29 00:16:27 +0100

    feat: Add categories to planned

---
+++ app/src/main/java/com/eventorium/presentation/event/fragments/BudgetCategoryFragment.java
@@ -0,0 +1,44 @@
+package com.eventorium.presentation.event.fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.eventorium.R;
+import com.eventorium.databinding.FragmentBudgetCategoryBinding;
+
+
+public class BudgetCategoryFragment extends Fragment {
+
+    private FragmentBudgetCategoryBinding binding;
+
+    public BudgetCategoryFragment() {
+    }
+
+    public static BudgetCategoryFragment newInstance() {
+        return new BudgetCategoryFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentBudgetCategoryBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        binding = null;
+    }
+}
--- app/src/main/java/com/eventorium/presentation/event/fragments/BudgetItemsFragment.java
+++ app/src/main/java/com/eventorium/presentation/event/fragments/BudgetItemsFragment.java
@@ -3,26 +3,47 @@ package com.eventorium.presentation.event.fragments;
 import static com.eventorium.presentation.event.fragments.BudgetPlanningFragment.ARG_EVENT_ID;
 import static com.eventorium.presentation.event.fragments.BudgetPlanningFragment.ARG_EVENT_TYPE;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
 import com.eventorium.R;
+import com.eventorium.data.category.models.Category;
 import com.eventorium.data.event.models.EventType;
 import com.eventorium.databinding.FragmentBudgetItemsBinding;
 import com.eventorium.databinding.FragmentBudgetPlanningBinding;
+import com.eventorium.presentation.category.viewmodels.CategoryViewModel;
+import com.eventorium.presentation.util.adapters.CategoryPagerAdapter;
+import com.eventorium.presentation.util.adapters.FavouritesPagerAdapter;
+import com.google.android.material.tabs.TabLayoutMediator;
 
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
 
+import dagger.hilt.android.AndroidEntryPoint;
+
+@AndroidEntryPoint
 public class BudgetItemsFragment extends Fragment {
 
     private FragmentBudgetItemsBinding binding;
+
+    private CategoryViewModel categoryViewModel;
+    private CategoryPagerAdapter adapter;
     private EventType eventType;
     private Long eventId;
+
+    private List<Category> plannedCategories = new ArrayList<>();
+    private List<Category> otherCategories = new ArrayList<>();
+
 
     public BudgetItemsFragment() {
     }
@@ -43,14 +64,63 @@ public class BudgetItemsFragment extends Fragment {
             eventId = getArguments().getLong(ARG_EVENT_ID);
             eventType = getArguments().getParcelable(ARG_EVENT_TYPE);
         }
+        ViewModelProvider provider = new ViewModelProvider(this);
+        categoryViewModel = provider.get(CategoryViewModel.class);
     }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentBudgetItemsBinding.inflate(inflater, container, false);
+        loadSuggestedCategories();
+        loadOtherCategories();
+
+        binding.btnAddCategory.setOnClickListener(v -> {
+            Category category = (Category) binding.categorySelector.getSelectedItem();
+            addCategory(category);
+        });
+
+        new TabLayoutMediator(binding.tabLayout, binding.viewPager, (tab, position) ->
+                tab.setText(adapter.getFragmentTitle(position))
+        ).attach();
+
         return binding.getRoot();
     }
+
+    private void loadOtherCategories() {
+        categoryViewModel.getCategories().observe(getViewLifecycleOwner(), categories -> {
+            //TODO: Add custom adapter, this adapter is useless like the whole android API
+            ArrayAdapter<Category> adapter = new ArrayAdapter<>(
+                    requireContext(),
+                    android.R.layout.simple_spinner_item,
+                    categories.stream()
+                            .filter(category -> !plannedCategories.contains(category))
+                            .toArray(Category[]::new)
+            );
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            binding.categorySelector.setAdapter(adapter);
+        });
+    }
+
+    private void addCategory(Category category) {
+        adapter.addFragment(BudgetCategoryFragment.newInstance(), category.getName());
+        plannedCategories.add(category);
+        otherCategories.remove(category);
+    }
+
+    private void loadSuggestedCategories() {
+        adapter = new CategoryPagerAdapter(this);
+        if(eventType != null) {
+            plannedCategories.addAll(eventType.getSuggestedCategories());
+            eventType.getSuggestedCategories()
+                    .forEach(category -> adapter.addFragment(
+                            BudgetCategoryFragment.newInstance(),
+                            category.getName())
+                    );
+        }
+        binding.viewPager.setAdapter(adapter);
+    }
+
 
     @Override
     public void onDestroy() {
